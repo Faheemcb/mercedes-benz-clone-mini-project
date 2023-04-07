92ca3852b72ca109efd5ec5263f37336a5e71de7
Mohammed Faheem <faheemmint978@gmail.com>
2023-04-07 10:21:31 +0530

    everything

---
+++ script.js
@@ -0,0 +1,24 @@
+ 
+ $(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+          $('#back-to-top').fadeIn();
+        } else {
+          $('#back-to-top').fadeOut();
+        }
+      });
+      $('#back-to-top').click(function () {
+        $('body,html').animate({ scrollTop: 0 }, 400);
+
+        return false;
+      });
+  });
+  
+  $(document).ready(function(){
+    $('.ubt').popover('hide');
+
+  })
+ 
+
+
+  
